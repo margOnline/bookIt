31b635f92e30437446351bbbdea26cd42789ddce
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-10-30 17:10:27 +0000

    Added fullcalendar

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    // page is now ready, initialize the calendar...
+
+    $('#calendar').fullCalendar({
+        header: {
+				left: 'prev,next today',
+				center: 'title',
+				right: 'month,basicWeek,basicDay'
+			}
+    })
+
+});
