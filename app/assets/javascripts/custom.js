0a610fd2856097fdefdb2e475139586df8921155
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-11-01 11:53:53 +0000

    AJAX patch request (almost) working

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -34,6 +34,26 @@ $(document).ready(function() {
 
     editable: true,
 
+    eventDrop: function(booking) {
+
+        function updateEvent(booking) {
+              $.ajax(
+                '/resources/'+current_resource()+'/bookings/'+booking.id,
+                { 'type': 'PATCH',
+
+                  data: { booking: { 
+                           starts_at: "" + booking.start,
+                           length: length
+                         } }
+                }
+              );
+          };
+
+        updateEvent(booking);
+
+      }
+    ,
+
    	dayClick: function(date, allDay, jsEvent, view) {
       // console.log(view.name);
       if (view.name === "month") {
@@ -43,32 +63,51 @@ $(document).ready(function() {
     },
 
  		select: function(start, end, allDay) {
+      if (window.location.href.match(/new/)) {
+        if(today_or_later()) {
+        	var length = (end-start)/(3600000);
 
-      if(today_or_later()) {
-      	var length = (end-start)/(3600000);
+          $('#calendar').fullCalendar('renderEvent', 
+            {
+              start: start,
+              end: end,
+              allDay: false
+            }
+          );
 
-        $('#calendar').fullCalendar('renderEvent', 
-          {
-            start: start,
-            end: end,
-            allDay: false
-          }
-        );
+          jQuery.post(
+            '/resources/'+current_resource()+'/bookings',
+            
+            { booking: {
+              start_time: start,
+              length: length,
 
-        jQuery.post(
-          '/resources/'+current_resource()+'/bookings',
-          
-          { booking: {
-            start_time: start,
-            length: length,
+          	} }
+          );
 
-        	} }
-        );
-
-  	    } else {
-          // alert("help!");
+    	    } else {
+            // alert("help!");
         }
       }
-		});
+       // else if (window.location.href.match(/edit/)) {
+      //   if(today_or_later()) {
+      //     var length = (end-start)/(3600000);
+
+      //     function updateEvent(the_event) {
+      //         $.update(
+      //           '/resources/'+current_resource()+'/bookings'+the_event.id,
+      //           { event: { 
+      //                      starts_at: "" + the_event.start,
+      //                      length: length
+      //                    }
+      //           }
+      //         );
+      //     };
+
+      //     } else {
+      //   }
+      // }
+    }
+	});
 
 });
