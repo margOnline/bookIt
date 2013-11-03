36466dacb12b7f0abeedee800a93c56d925f3c33
Jonathan Tsang <chewymeister88@googlemail.com>
2013-11-03 13:49:15 +0000

    configured filter_paramater_logging, tests don't log ActiveRecord requests, rspec and cucumber to pretty

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -2,3 +2,5 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += [:password]
+# Set this to zero when you want full debugging logs
+Rails.logger.level = 4
