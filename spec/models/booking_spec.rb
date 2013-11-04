acea543edbf49c5fe7234b7b3625a35ad3d10c7e
Jonathan Tsang <chewymeister88@googlemail.com>
2013-11-04 11:50:24 +0000

    Added test for when bookings start straight after another, when booking has same start and end time as another. Test passes

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -6,28 +6,28 @@ describe Booking do
     Timecop.travel(t)
   end
   let!(:resource) { Resource.new(name: 'Court 1') }
-  let!(:booking) { Booking.new(start_time: DateTime.now, length: 1) }
+  let!(:booking) { Booking.new(start_time: DateTime.now + 15.minutes, length: 1) }
 
   subject{ booking }
 
   it { should respond_to(:start_time) }
 
   context "should not be valid when" do
-    it "a length is not provided" do
+    it "the length is not provided" do
       booking = Booking.new(start_time: DateTime.now)
       add_reference resource, booking
 
       expect(booking).to_not be_valid
     end
 
-    it "a start time is not provided" do
+    it "the start time is not provided" do
       booking = Booking.new(length: 1)
       add_reference resource, booking
 
       expect(booking).to_not be_valid
     end
 
-    it "both a start and a length are not provided" do
+    it "both the start and the length are not provided" do
       booking = Booking.new()
       add_reference resource, booking
 
@@ -47,10 +47,19 @@ describe Booking do
 
       expect(booking).to_not be_valid
     end
+
+    it "the start time and the end time are the same as those of a previous booking" do
+      booking_1 = Booking.new( start_time: DateTime.now, length: 1 )
+      booking_2 = Booking.new( start_time: DateTime.now, length: 1 )
+      add_reference resource, booking_2
+      add_reference resource, booking_1
+
+      expect(booking_2).to_not be_valid
+    end
   end
 
-  context 'should be valid with' do
-    it 'a start time, length, and resource' do
+  context 'should be valid when' do
+    it 'the start time, length, and resource are provided' do
       booking = Booking.new(start_time: DateTime.now + (1.hours), length: 1)
       add_reference resource, booking
 
@@ -63,13 +72,22 @@ describe Booking do
 
       expect(booking).to be_valid
     end
+
+    it 'when a booking starts immediately after one ends' do
+      add_reference resource, booking
+      booking_2 = Booking.new(start_time: DateTime.now + (1.hours) + 15.minutes, length: 1)
+      add_reference resource, booking_2
+
+      expect(booking).to be_valid
+      expect(booking_2).to be_valid
+    end
   end
 
   it 'calculates an end time from the start time and the length' do
     booking.start_time = DateTime.new(2013,10,30,9,9,0)
     booking.length = 2
 
-    expect(booking.calculate_end_time).to eq (booking.start_time + (2.hours))
+    expect(booking.calculate_end_time).to eq (booking.start_time + (2.hours - 60))
   end
 
   
